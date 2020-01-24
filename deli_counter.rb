e53741009914fae03e1365efba0b67c92a951ef3
Khalid T <khalid.tissire@gmail.com>
2020-01-24 06:32:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,6 +20,6 @@ def line(katzDeliLine)
   if (len > 0)
     katzDeliLine.each_with_index { |n,i| puts "The line is currently: #{i+1}. #{n}" }
   else
-    
+    puts "The line is currently empty."
   end
 end
